cc6e7f44b04115be315379762b2eba2a02dd389c
Centennial <harsimrankaur708@gmail.com>
2020-08-07 23:47:03 -0400

    payroll.py completed

--- payroll.py
+++ payroll.py
@@ -1,9 +1,12 @@
-
+#format for printing the record
 print(f"Employee no\t| Employee Name\t\t| Hours worked")
 print("")
 
+#opening file in readable format
 f=open("data.txt","r")
 for lines in f.readlines():
+
+    #using split function to split the string
     substr=lines.split(",")
     print(f"{substr[0]}\t\t|{substr[1]} {substr[2]}\t\t|{substr[3]}")
 
